4555a7de842d4d31666a0d83b4426b30d6ea6009
AbhishekKr <abhikumar163@gmail.com>
2024-09-06 16:07:01 +0530

    add Heads Up Display to start/game-over/score
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,29 +4,24 @@ extends Node
 var score
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	new_game()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 
 func new_game():
+	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Careful now..")
 
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout() -> void:
